b20c6a6907ff6187c08491b79ebcf3ffba589403
Cem GÖKSU <127553434+cemgoksu35@users.noreply.github.com>
2024-09-22 22:36:40 +0300

    version 1

---
+++ PortfolioProjectNight/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PortfolioProjectNight.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public int MyProperty { get; set; }
+        
+
+        public ActionResult CategoryList()
+        {
+             
+            return View();
+        }
+        public ActionResult CreateCategory()
+        {
+            return View();
+        }
+
+
+    }
+}
